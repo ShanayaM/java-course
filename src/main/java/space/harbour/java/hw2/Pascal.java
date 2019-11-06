243d4e2823c28349b35128a253d180fd9c58156e
ShanayaM <msdec.ict@gmail.com>
2019-11-06 13:38:51 +0100

    Update Pascal.java

--- src/main/java/space/harbour/java/hw2/Pascal.java
+++ src/main/java/space/harbour/java/hw2/Pascal.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class Pascal {
 
-    static void printTriangle (int n) {
+    static void printTriangle(int n) {
 
         for (int line = 1; line <= n; line++) {
             int c = 1;
             for (int i = 1; i <= line; i++) {
                 System.out.print(c + " ");
-                c = c*(line - i) / i;;
+                c = c * (line - i) / i;
             }
             System.out.println();
         }
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter N: ");
