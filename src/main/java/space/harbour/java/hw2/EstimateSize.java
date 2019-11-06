77f43863cec436d1ef8afd2d1c9801126f3171e1
ShanayaM <msdec.ict@gmail.com>
2019-11-06 16:19:31 +0100

    Updated code style

--- src/main/java/space/harbour/java/hw2/EstimateSize.java
+++ src/main/java/space/harbour/java/hw2/EstimateSize.java
@@ -13,24 +13,25 @@ final class EstimateSize {
 
     public static void main(String[] args) {
         System.gc();
-        int[] i = new int[160000000];  // int
-        System.out.println(Runtime.getRuntime().totalMemory() -
-                Runtime.getRuntime().freeMemory());
-        Thread.sleep(10);
+        int[] i = new int[160_000_000];  // int
+        int sleepTime = 10;
+        System.out.println(Runtime.getRuntime().totalMemory()
+                - Runtime.getRuntime().freeMemory());
+        Thread.sleep(sleepTime);
 
         Integer integer = null;    // reference
-        System.out.println(Runtime.getRuntime().totalMemory() -
-                Runtime.getRuntime().freeMemory());
-        Thread.sleep(10);
+        System.out.println(Runtime.getRuntime().totalMemory()
+                - Runtime.getRuntime().freeMemory());
+        Thread.sleep(sleepTime);
 
         String string = null;      // string
-        System.out.println(Runtime.getRuntime().totalMemory() -
-                Runtime.getRuntime().freeMemory());
-        Thread.sleep(10);
+        System.out.println(Runtime.getRuntime().totalMemory()
+                - Runtime.getRuntime().freeMemory());
+        Thread.sleep(sleepTime);
 
         Object object = null;      // object
-        System.out.println(Runtime.getRuntime().totalMemory() -
-                Runtime.getRuntime().freeMemory());
-        Thread.sleep(10);
+        System.out.println(Runtime.getRuntime().totalMemory()
+                - Runtime.getRuntime().freeMemory());
+        Thread.sleep(sleepTime);
     }
 }
