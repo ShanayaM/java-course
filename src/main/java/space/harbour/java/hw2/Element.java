cbb16756a8ba38a8b80b28d4434c0694d75d78b7
ShanayaM <msdec.ict@gmail.com>
2019-11-07 14:34:51 +0100

    Added hw3 files

---
+++ src/main/java/space/harbour/java/hw2/Element.java
@@ -0,0 +1,28 @@
+package main.java.space.harbour.java.hw3;
+
+import java.util.Map;
+
+public class Element<K, V> implements Map.Entry<K, V> {
+    private K key;
+    private V value;
+
+    public Element(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public K getKey() {
+        return key;
+    }
+
+    @Override
+    public V getValue() {
+        return value;
+    }
+
+    @Override
+    public V setValue(V value) {
+        return this.value = value;
+    }
+}
